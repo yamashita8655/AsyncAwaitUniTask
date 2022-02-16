4128c982eaee9e09e9e6ce8f8b6b67d2c1c4d3cd
noa_yamashita <t.yamashita@noa-tec.co.jp>
2022-02-17 07:26:34 +0900

    async/awaitのサンプル処理記述

--- AsyncAwaitUnitask/Assets/AsyncAwaitTest.cs
+++ AsyncAwaitUnitask/Assets/AsyncAwaitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class AsyncAwaitTest : MonoBehaviour
@@ -8,30 +9,54 @@ public class AsyncAwaitTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CoroutineTest();
+//        CoroutineTestFunction();
+        AsyncAwaitTestFunction();
     }
 
     // コルーチンの場合の非同期
-    private void CoroutineTest()
+    private void CoroutineTestFunction()
     {
         Debug.Log("メイン関数開始");
         Debug.Log("処理待ち開始");
         StartCoroutine(
-            CoCoroutine(
-                () => {
-                    Debug.Log("処理終了");
+            CoFunction(
+                (returnString) => {
+                    Debug.Log("処理終了:"+returnString);
                 }
             )
         );
         Debug.Log("メイン関数終了");
     }
 
-    private IEnumerator CoCoroutine(Action endCallback)
+    private IEnumerator CoFunction(Action<string> endCallback)
     {
         for (int i = 0; i < 5; i++) {
             yield return new WaitForSeconds(1);
             Debug.Log("処理中:" + i);
         }
-        endCallback();
+        endCallback("returnStrings");
+    }
+
+    // Async/Awaitの場合
+    private async void AsyncAwaitTestFunction()
+    {
+        Debug.Log("メイン関数開始");
+        Task<string> task = AsyncAwaitFunctionReturnString();
+        Debug.Log("処理待ち開始");
+        await task;
+        //        string returnString = await task;
+        string returnString = "";
+        Debug.Log("処理終了:" + returnString);
+        Debug.Log("メイン関数終了");
+    }
+
+    private async Task<string> AsyncAwaitFunctionReturnString()
+    {
+        for (int i = 0; i < 5; i++) {
+            await Task.Delay(1000);
+            Debug.Log("処理中:" + i);
+        }
+
+        return "returnStrings";
     }
 }
